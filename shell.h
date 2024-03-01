da1dfbbd2cc2756686df4096d758b4ad1f047baa
programmingwithjerry <eneyellowj@gmail.com>
2024-03-01 20:40:00 +0000

    shell.h

--- shell.h
+++ shell.h
@@ -2,8 +2,6 @@
 #define SHELL_H
 
 
-
-
 #include <stdarg.h>
 #include <errno.h> /*for perror*/
 
@@ -20,11 +18,17 @@
 #define MAX_ALIASES 100
 #define BUFFER_SIZE 1024
 
+struct Alias
+{
+	char *name;
+	char *value;
+};
 
-
+struct Alias aliases[MAX_ALIASES];
 
 int main(int argc, char *argv[]);
 void prompt_user(void);
+void our_get_command(char *command, size_t size);
 /*void getAndPrintCommand();*/
 int isjerry_print(const char *format, ...);
 /*void execute_command(const char *command);*/
@@ -35,7 +39,8 @@ void handleCommandLine(int argc, char *argv[]);
 /*void exit_shell();*/
 /*void handle_exit_cmd(const char *input);*/
 void isjerry_print_env_built_in(void);
-void isjerry_execute_exit_command(void);
+void execute_exit_command(void);
+/*void isjerry_execute_exit_command(void);*/
 void execute_path_command(const char *command);
 void isjerry_semicolon_handler(char *input);
 void isjerry_ampersound_handler(char *input);
@@ -48,6 +53,11 @@ int our_unsetenv_builtin(const char *variable);
 int isjerry_setenv_builtin(const char *variable, const char *value);
 int our_cd_builtin(char *directory);
 
+/*aliases*/
+void our_print_all_aliases(void);
+void our_print_specific_aliases(char *names[], int count);
+void our_define_alias(char *name, char *value);
+void our_alias_builtin(int argc, char *argv[]);
 
 
 #endif
